33b53f89090684730b6a37cf4a8b6fe09adf1756
Abi L <124858748+lailiel@users.noreply.github.com>
2023-03-19 21:17:56 -0500

    finished code

--- script.js
+++ script.js
@@ -4,59 +4,47 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
+  var characters = '';
+  var password = '';
+
   var passwordLength = window.prompt("Enter password length. (8-128)")
   if (passwordLength < 8 || passwordLength > 128) {
-    var invalid = window.confirm("Invalid length entry")
-  
+    window.alert("Invalid length entry");
+    return;
   }
 
+  var lowercase = window.confirm("Include lower case letters?")
+  var uppercase = window.confirm("Include upper case letters?")
+  var numbers = window.confirm("Include numbers ?")
+  var symbol = window.confirm("Include special characters?")
 
   var letterLC = "abcdefghijklmnopqrstuvwxyz";
   var letterUC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbersChar = "0123456789";
   var specChar = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 
-  
-  var lowercase = window.confirm("Include lower case letters?")
-  var uppercase = window.confirm("Include upper case letters?")
-  var numbers = window.confirm("Include numbers ?")
-  var symbol = window.confirm("Include special characters?")
-
-  var includes= "";
-
   if (lowercase) {
-    includes +=letterLC
+    characters += letterLC;
   }
   if (uppercase) {
-    includes +=letterUC
+    characters += letterUC;
   }
   if (numbers) {
-    includes +=numbersChar
+    characters += numbersChar;
   }
   if (symbol) {
-    includes +=specChar
+    characters += specChar;
   }
 
-  // figure out a way for the length to be equal to the of all icnluded character options.
   for (var i = 0; i < passwordLength; i++) {
-    var randomChar = Math.floor(Math.random() * (letterUC.length + letterLC.length + numbersChar.length + specChar.length).length);
-    password += includes[randomChar];
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-
-
-for (var i = 0; i < [passwordLength]; i++) {
-    console.log(includes);
-}
-
-  var password = writePassword() 
-
 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
